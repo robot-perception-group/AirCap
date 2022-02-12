25f4b5ec456f56ef45a0cc843de1d9338a8dc700
local user on lta robot <none@invalid>
2022-02-12 17:10:53 +0100

    changed camera orientation to match actual camera (only 30 deg down)

--- scripts/on_blimp/video.sh
+++ scripts/on_blimp/video.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rosrun videograb videograb_node /machine_$MACHINE ${LOGDIR}/current/ 20 3 __ns:=/machine_$MACHINE __name:=videograb_node_$MACHINE
+rosrun videograb videograb_node /machine_$MACHINE ${LOGDIR}/current/ 20 4 __ns:=/machine_$MACHINE __name:=videograb_node_$MACHINE
 
 #echo "video capture and recording failed with errorcode $?" >>${LOGDIR}/current/faillog
